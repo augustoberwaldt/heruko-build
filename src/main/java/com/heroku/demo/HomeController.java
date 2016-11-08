b1f4b2e377288ed1e9dd8c7cdf1b1f8e6fcc1c88
Douglas Ballester <douglas.ballester@gmail.com>
2016-11-07 23:57:56 -0200

    foi

--- src/main/java/com/heroku/demo/HomeController.java
+++ src/main/java/com/heroku/demo/HomeController.java
@@ -25,7 +25,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 
 	@RequestMapping("/")
-	@ResponseBody
 	public String home() {
 				
 		return	 "index";
