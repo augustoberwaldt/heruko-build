d4ebff524e9039c3442953700e3115a6f1a3d5f4
Cassiano <cassianodoneda@gmail.com>
2016-11-14 13:06:03 -0200

    Adiciona Link Edição Banca

--- src/main/java/com/heroku/demo/HomeController.java
+++ src/main/java/com/heroku/demo/HomeController.java
@@ -49,6 +49,12 @@ public class HomeController {
 		return	 "defbanca";
     }
 	
+	@RequestMapping("/defbancaedit")
+	public String defbancaedit() {
+				
+		return	 "defbancaedit";
+    }
+	
 	@RequestMapping("/blankpage")
 	public String blankpage() {
 				
