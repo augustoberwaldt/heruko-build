ee6cbc21ef29956dd3188b1789f8a4b13775fc06
Cassiano <cassianodoneda@gmail.com>
2016-11-20 01:50:47 -0200

    Create novaProposta

--- src/main/java/com/heroku/demo/HomeController.java
+++ src/main/java/com/heroku/demo/HomeController.java
@@ -96,8 +96,7 @@ public class HomeController {
     }
 	
 	@RequestMapping("/novaProposta")
-	public String novaProposta() {
-				
+	public String novaProposta() {				
 		return	 "novaProposta";
     }
 	
